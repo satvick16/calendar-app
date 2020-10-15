3b0191ec8a5c04f56dadfc2f28019eabd50c689a
Satvick Acharya <satvickacharya16@gmail.com>
2020-10-15 10:05:42 -0400

    initial commit

---
+++ flaskblog/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flaskblog import db
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Event('{self.title}', '{self.date_posted}')"
---
+++ flaskblog/routes.py
@@ -0,0 +1,62 @@
+import os
+import secrets
+from flask import render_template, url_for, flash, redirect, request, abort
+from flaskblog import app, db
+from flaskblog.forms import *
+from flaskblog.models import *
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    events = Event.query.order_by(Event.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', events=events)
+
+
+@app.route("/new-event", methods=['GET', 'POST'])
+@login_required
+def new_event():
+    form = EventForm()
+    if form.validate_on_submit():
+        event = Event(title=form.title.data, content=form.content.data)
+        db.session.add(event)
+        db.session.commit()
+        flash('Your event has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('new_event.html', title='New Event',
+                           form=form, legend='New Event')
+
+
+@app.route("/post/<int:event_id>")
+def event(event_id):
+    event = Event.query.get_or_404(event_id)
+    return render_template('event.html', title=event.title, event=event)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    form = EventForm()
+    if form.validate_on_submit():
+        event.title = form.title.data
+        event.content = form.content.data
+        db.session.commit()
+        flash('Your event has been updated!', 'success')
+        return redirect(url_for('event', post_id=event.id))
+    elif request.method == 'GET':
+        form.title.data = event.title
+        form.content.data = event.content
+    return render_template('new_event.html', title='Update Event',
+                           form=form, legend='Update Event')
+
+
+@app.route("/event/<int:event_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    event = Event.query.get_or_404(post_id)
+    db.session.delete(event)
+    db.session.commit()
+    flash('Your event has been deleted!', 'success')
+    return redirect(url_for('home'))
